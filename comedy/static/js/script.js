01f36bec66bf7fc19da7a823b3b52b17530369c0
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-23 16:36:57 +0300

    adding a bit of interactivity

--- comedy/static/js/script.js
+++ comedy/static/js/script.js
@@ -3,22 +3,43 @@ window.onload = function () {
 	  delimiters: ['[[', ']]'],
 	  el: '#app',
 	  data: {
-	    messages: [{'text': 'Hey there', 'user': true, 'chat_bot': false}, {'text': 'brown cow', 'user': false, 'chat_bot': true}],
+	    messages: [],
 	    input: '',
 	    status: true,
+	    send_blank: false,
+	    placeholder: 'Send a message to the chatbot...',
 	  },
 	  created: function() {
 	  	
 	  },
 	  methods: {
 		add_message: function() {
-			var message = {
-				'text': this.input,
-				'user': true,
-				'chat_bot': false,
-			};
-			this.messages.push(message);
-			this.input = '';
+			if (this.input.length > 0) {
+				var message = {
+					'text': this.input,
+					'user': true,
+					'chat_bot': false,
+				};
+				this.messages.push(message);
+				this.input = '';
+
+				//just incase
+				this.send_blank = false;
+				this.placeholder = "Send a message to the chatbot..."
+			} else {
+				this.send_blank = true;
+				this.placeholder = "Please put in some text"
+			}
+
+		},
+		check_content: function() {
+			if (this.input.length > 0) {
+				this.send_blank = false;
+				this.placeholder = "Send a message to the chatbot..."
+			} else {
+				this.send_blank = true;
+				this.placeholder = "Please put in some text"
+			}
 		},
 	  }
 	});
