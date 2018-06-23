31cb0892da052ca1c86e8c5ae268aefba3c5ec3d
Samuel Muiruri <muiruri.samuel@gmail.com>
2018-06-23 15:10:02 +0300

    add classes based on message

--- comedy/static/js/script.js
+++ comedy/static/js/script.js
@@ -3,13 +3,21 @@ window.onload = function () {
 	  delimiters: ['[[', ']]'],
 	  el: '#app',
 	  data: {
-	    messages: ['Hey there', 'brown cow'],
+	    messages: [{'text': 'Hey there', 'user': true, 'chat_bot': false}, {'text': 'brown cow', 'user': false, 'chat_bot': true}],
+	    input: '',
 	  },
 	  created: function() {
 	  	
 	  },
 	  methods: {
-		
+		add_message: function() {
+			var message = {
+				'text': this.input,
+				'user': true,
+				'chat_bot': false,
+			}
+			this.messages.push()
+		},
 	  }
 	});
 };
